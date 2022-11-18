5ba22b0804dbe93d1b9a1ec992ce3bfceb9b23a4
yuyatinnefeld <yuyawamo@gmail.com>
2022-11-18 09:28:11 +0100

    feat: update sql db with postgres

--- service/database.py
+++ service/database.py
@@ -1,14 +1,29 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
+instance_name = "postgresql-instance"
+user_name = "postgres"
+password = "password"
+db_name = "postgres"
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./service.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+env = os.environ['ENV']
+
+if env == "dev":
+    private_ip = "127.0.0.1" # for the local-testing with proxy
+    port = "3306" # for the local-testing with proxy
+else:
+    private_ip = "10.08.15.10"
+    port = "5432"
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./service.db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user_name}:{password}@{private_ip}:{port}/{db_name}"
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
