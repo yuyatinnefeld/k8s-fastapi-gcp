111878745d8820bd620e2d68d83af9e787203ec6
yuyatinnefeld <yuyawamo@gmail.com>
2022-12-03 16:24:42 +0100

    fix: update tf variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,6 @@ terraform {
 
 module "cloud_sql" {
   source                = "./modules/cloud_sql"
-  region                = var.region
   db_instance_name      = var.db_instance_name
   network_name          = var.network_name
   db_name               = var.db_name
--- terraform/modules/var.tf
+++ terraform/modules/var.tf
@@ -1,3 +1,17 @@
+### GENERAL ENV ###
+variable project_id {
+  type = string
+}
+
+variable env {
+  type = string
+}
+
+variable region {
+  type = string
+  default = "europe-west1"
+}
+
 variable db_instance_name {
   type = string
 }
