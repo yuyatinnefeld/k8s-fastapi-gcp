52e7d1d9c78fc59cbcc1d9c06ad10005b12b467d
yuyatinnefeld <yuyawamo@gmail.com>
2022-12-03 16:53:17 +0100

    feat: activate validate plan stages

--- env_dev.sh
+++ env_dev.sh
@@ -1,7 +1,7 @@
 export PROJECT_ID="yuyatinnefeld-dev"
 export CLUSTER_NAME="my-fastapi-node"
 export VPC_NETWORK="vpc-mainnet"
-export INSTANCE_NAME="my-postgresql-instance"
+export INSTANCE_NAME="postgresql-db-instance"
 export APP_COLOR="blue"
 export APP_MODE="dev"
 export DB_NAME="postgres"
