273ed0de8dc03e734c7b13a6cac86c31b9e08c97
yuyatinnefeld <yuyawamo@gmail.com>
2022-12-03 16:26:52 +0100

    fix: update tf variables

--- terraform/var.tf
+++ terraform/var.tf
@@ -3,13 +3,13 @@ variable project_id {
   type = string
 }
 
-variable env {
-  type = string
-}
-
 variable region {
   type = string
   default = "europe-west1"
+}
+
+variable env {
+  type = string
 }
 
 variable db_instance_name {
