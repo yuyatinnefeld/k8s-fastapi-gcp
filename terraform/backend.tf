63dbfee71aa4d6f49a391f9d898ed95066cedcb2
yuyatinnefeld <yuyawamo@gmail.com>
2023-01-02 07:02:35 +0100

    refactor: simplifile cred setup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
     bucket      = "yuyatinnefeld-ENVIRONMENT-tf-state"
-    prefix      = "state/resources"
-    credentials = "./creds/serviceaccount.json"
+    prefix      = "state/test"
   }
 }
