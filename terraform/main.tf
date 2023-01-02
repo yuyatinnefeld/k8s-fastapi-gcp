38903a9229cf1f3b65ee1043c9472999052aa198
yuyatinnefeld <yuyawamo@gmail.com>
2023-01-02 07:25:44 +0100

    feat: reactivate cloud sql moudle

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,13 +17,12 @@ module "cloud_storage" {
   env              = var.env
 }
 
-
-# module "cloud_sql" {
-#   source                = "./modules/cloud_sql"
-#   region                = var.region
-#   db_instance_name      = var.db_instance_name
-#   network_name          = var.network_name
-#   db_name               = var.db_name
-#   db_user               = var.db_user
-#   db_password           = var.db_password
-# }
+module "cloud_sql" {
+  source                = "./modules/cloud_sql"
+  region                = var.region
+  db_instance_name      = var.db_instance_name
+  network_name          = var.network_name
+  db_name               = var.db_name
+  db_user               = var.db_user
+  db_password           = var.db_password
+}
